6c830219f0162cfa689bf935b532a287e5fe6f32
vandilsonbrito <vandilson25@outlook.com>
2024-11-27 18:29:56 -0300

    build: create next-project-with-tests-boilerplate

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+export default {
+  // Lint & Prettier TS and JS files
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `prettier --write ${filenames.join(' ')}`,
+    `npm run lint --fix . ${filenames.join(' --file')}`
+  ]
+}
---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  }
+}
