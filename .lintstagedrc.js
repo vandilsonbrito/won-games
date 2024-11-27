0f30591db6946fcbb72a0b1cacba8ef1da61be03
vandilsonbrito <vandilson25@outlook.com>
2024-11-27 19:29:04 -0300

    chore: add test to eslint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,7 @@ export default {
   // Lint & Prettier TS and JS files
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
-    `npm run lint --fix . ${filenames.join(' --file')}`
+    `npm run lint --fix . ${filenames.join(' --file')}`,
+    `npm run test -- --findRelatedTests ${filenames.join(' ')}`
   ]
 }
