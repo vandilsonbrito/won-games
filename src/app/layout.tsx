1368ade5cf3114f3c7d270185cab16b4c1dcc573
vandilsonbrito <vandilson25@outlook.com>
2024-11-27 21:09:46 -0300

    chore: install and configure styled-component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
+import StyledComponentsRegistry from '@/lib/registry'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className}`}>{children}</body>
+      <body className={`${montserrat.className}`}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
